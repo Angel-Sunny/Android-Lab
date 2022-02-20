f059c11db0d7cbb7faf4e60e393096e3dad2181d
Angel-Sunny <77189803+Angel-Sunny@users.noreply.github.com>
2022-02-21 00:55:14 +0530

    4ComplexCalculator

--- MainActivity.java
+++ MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.a3simplecalcu;
+package com.example.a4calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,63 +6,167 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    float res=0;
-    EditText n1;
-    EditText n2;
-    Button ba;
-    Button bs;
-    Button bm;
-    Button bd;
+
+    Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b0,plus,minus,div,equal,mul;
+    boolean Addition, Subtract, Multiplication,Division;
+    EditText e1;
+    float mValueOne, mValueTwo;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        n1=findViewById(R.id.num1);
-        n2=findViewById(R.id.num2);
-        ba=findViewById(R.id.add);
-        bs=findViewById(R.id.sub);
-        bm=findViewById(R.id.mul);
-        bd=findViewById(R.id.div);
+        e1 = findViewById(R.id.enternumber);
+        b1 = findViewById(R.id.button);
+        b2 = findViewById(R.id.button2);
+        b3 = findViewById(R.id.button3);
+        b4 = findViewById(R.id.button4);
+        b5 = findViewById(R.id.button5);
+        b6 = findViewById(R.id.button6);
+        b7 = findViewById(R.id.button7);
+        b8 = findViewById(R.id.button8);
+        b9 = findViewById(R.id.button9);
+        plus = findViewById(R.id.button10);
+        b0 = findViewById(R.id.button11);
+        minus = findViewById(R.id.button12);
+        mul = findViewById(R.id.button13);
+        div = findViewById(R.id.button14);
+        equal = findViewById(R.id.button15);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                e1.setText(e1.getText() + "1");
 
 
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                e1.setText(e1.getText() + "2");
+            }
+        });
 
-        ba.setOnClickListener(new View.OnClickListener() {
+        b3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                float a=Float.parseFloat(n1.getText().toString());
-                float b=Float.parseFloat(n2.getText().toString());
-                res=a+b;
-                Toast.makeText(MainActivity.this, "Sum="+res, Toast.LENGTH_SHORT).show();
+                e1.setText(e1.getText() + "3");
             }
         });
-        bs.setOnClickListener(new View.OnClickListener() {
+
+        b4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                float a=Float.parseFloat(n1.getText().toString());
-                float b=Float.parseFloat(n2.getText().toString());
-                res=a-b;
-                Toast.makeText(MainActivity.this, "Difference="+res, Toast.LENGTH_SHORT).show();
+                e1.setText(e1.getText() + "4");
             }
         });
-        bm.setOnClickListener(new View.OnClickListener() {
+        b5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                float a=Float.parseFloat(n1.getText().toString());
-                float b=Float.parseFloat(n2.getText().toString());
-                res=a*b;
-                Toast.makeText(MainActivity.this, "Product="+res, Toast.LENGTH_SHORT).show();
+                e1.setText(e1.getText() + "5");
             }
         });
-        bd.setOnClickListener(new View.OnClickListener() {
+
+        b6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                float a=Float.parseFloat(n1.getText().toString());
-                float b=Float.parseFloat(n2.getText().toString());
-                res=a/b;
-                Toast.makeText(MainActivity.this, "Quotient="+res, Toast.LENGTH_SHORT).show();
+                e1.setText(e1.getText() + "6");
+            }
+        });
+
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                e1.setText(e1.getText() + "7");
+            }
+        });
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                e1.setText(e1.getText() + "8");
+            }
+        });
+
+        b9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                e1.setText(e1.getText() + "9");
+            }
+        });
+
+        b0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                e1.setText(e1.getText() + "0");
+            }
+        });
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (e1 == null) {
+                    e1.setText("");
+                } else {
+                    mValueOne = Float.parseFloat(e1.getText() + "");
+                    Addition = true;
+                    e1.setText(null);
+                }
+            }
+        });
+
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(e1.getText() + "");
+                Subtract = true;
+                e1.setText(null);
+            }
+        });
+        mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(e1.getText() + "");
+                Multiplication = true;
+                e1.setText(null);
+            }
+        });
+
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(e1.getText() + "");
+                Division = true;
+                e1.setText(null);
+            }
+        });
+        equal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueTwo = Float.parseFloat(e1.getText() + "");
+
+                if (Addition == true) {
+                    e1.setText(mValueOne + mValueTwo + "");
+                    Addition = false;
+                }
+
+                if (Subtract == true) {
+                    e1.setText(mValueOne - mValueTwo + "");
+                    Subtract = false;
+                }
+
+                if (Multiplication == true) {
+                    e1.setText(mValueOne * mValueTwo + "");
+                    Multiplication = false;
+                }
+
+                if (Division == true) {
+                    e1.setText(mValueOne / mValueTwo + "");
+                    Division = false;
+                }
             }
         });
     }
