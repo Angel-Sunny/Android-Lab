b427e15fde3404bbfc85b020b79750f8094ee0d7
Angel-Sunny <77189803+Angel-Sunny@users.noreply.github.com>
2022-02-21 01:02:57 +0530

    7Intent

--- MainActivity2.java
+++ MainActivity2.java
@@ -7,17 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    Button b1;
+public class MainActivity2 extends AppCompatActivity {
+    Button b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        b1=findViewById(R.id.button);
-        b1.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main2);
+        b2=findViewById(R.id.button3);
+        b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i=new Intent(getApplicationContext(),MainActivity2.class);
+                Intent i=new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(i);
             }
         });
